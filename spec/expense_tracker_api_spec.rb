689d9c2716299d7ae95e065c834c2d60d42b88ab
turnspike <hello@turnspike.com>
2019-02-15 14:46:28 +1030

    Add guard gem, fix bundler versioning

---
+++ spec/expense_tracker_api_spec.rb
@@ -0,0 +1,21 @@
+require 'rack/test'
+require 'json'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+      #foo oo
+    end
+
+    it 'records submitted expenses' do
+      coffee = {
+          'payee' => 'Starbucks', 'amount' => 5.75,
+          'date' => '2017-06-10'
+      }
+      post '/expenses', JSON.generate(coffee)
+    end
+  end
+end
